593a7eb73527223278a3773a96ad733c9089e42f
SuchithTissera <suchithtissera@gmail.com>
2026-04-12 18:46:13 +0530

    Add gallery path to URLs

--- salon/urls.py
+++ salon/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('booking/', views.booking_page, name='booking'),
+    path('gallery/', views.gallery, name='gallery'),
 ]
